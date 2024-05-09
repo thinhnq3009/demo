b992b73758a18309252402e64755e6778695cb52
Nguyen Quoc Thinh <thinhnq3009@gmail.com>
2024-05-09 11:00:41 +0700

    Update model

--- src/components/model/ModelPreview.tsx
+++ src/components/model/ModelPreview.tsx
@@ -5,11 +5,11 @@ import { Suspense, useState } from 'react';
 import { AnimationMixer } from 'three';
 
 const Character = () => {
-  const { scene: character, animations } = useGLTF('/assets/models/Soldier.glb');
+  const { scene: character, animations } = useGLTF('/assets/models/emily.glb');
   console.log(animations);
   console.log(character);
   const mixer = new AnimationMixer(character);
-  const action = mixer.clipAction(animations[0]);
+  const action = mixer.clipAction(animations[2]);
   action.play();
   const [y] = useState(-2.6);
   const [s] = useState(3.2);
