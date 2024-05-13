14c15a1663d92e7ad7c38600bc1e48177b78b9e1
ThaiNguyen1710 <thai.nguyen.171002@gmail.com>
2024-05-13 09:20:12 +0700

    fixui

--- src/app/(mini-app)/page.tsx
+++ src/app/(mini-app)/page.tsx
@@ -6,6 +6,7 @@ import ButtonImageArrow from '@/components/button/ButtonImageArrow';
 import CaseInfo from '@/components/common/CaseInfo';
 import HomeMenu from '@/components/common/menu/HomeMenu';
 import ChatPopup from '@/components/common/ChatPopup';
+import Link from 'next/link';
 
 const avt = '/assets/avatar/avt-2.jpg';
 
@@ -15,7 +16,9 @@ export default function HomePage() {
         <header className="font-mochi flex justify-between">
             <Profile coin={100} avatar={avt} ton={100} username={'Username'}/>
             <div className="flex flex-col gap-2">
+                <Link href="/wallet">
                 <ButtonImageSquare image="\assets\item\wallet.svg" className="bg-[#2B0940]">Wallet</ButtonImageSquare>
+                </Link>
                 <ButtonImageSquare image="\assets\item\battle.svg" className="bg-[#971C01]">Battle</ButtonImageSquare>
             </div>
         </header>
