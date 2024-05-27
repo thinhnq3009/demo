f280424c3e1cbe0f017f5edfcf54fb42c155ddf6
Nguyen Quoc Thinh <thinhnq3009@gmail.com>
2024-05-27 16:07:08 +0700

    disable eslint rule

--- src/app/game/scenes/StatGameObject.ts
+++ src/app/game/scenes/StatGameObject.ts
@@ -1,28 +1,54 @@
 import Phaser, { NONE } from 'phaser';
-import Stat from '~/data/stat';
+import Stat from '@/app/game/data/stat';
 
 export default class StatGameObject extends Phaser.GameObjects.Container {
   // Add properties for the stats of the object
-  public stat:Stat;
+  public stat: Stat;
 
   private current_stone = NONE;
 
-  constructor(scene: Phaser.Scene, x: number, y: number, texture: string, stat:Stat) {
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string, stat: Stat) {
     super(scene, x, y);
     const text_value_offset = 140;
     const y_text_offset = 10;
     this.stat = stat;
     const image = scene.add.image(x, y, texture);
     this.add(image);
-    const text_type = scene.add.text(x - text_value_offset, y - y_text_offset, this.convert_to_short_string(stat.type), 
-      { fontFamily: 'Mochiy Pop One', fontSize: '14px', color:this.convert_to_type_to_color(stat.type), stroke: '##8B661E', strokeThickness: 1 },
+    const text_type = scene.add.text(x - text_value_offset, y - y_text_offset, this.convert_to_short_string(stat.type),
+      {
+        fontFamily: 'Mochiy Pop One',
+        fontSize: '14px',
+        color: this.convert_to_type_to_color(stat.type),
+        stroke: '##8B661E',
+        strokeThickness: 1,
+      },
     );
     this.add(text_type);
-    const text_value = scene.add.text(x, y - y_text_offset, stat.value.toString(), { fontFamily: 'Mochiy Pop One', fontSize: '14px', color:'#fff', stroke: '#8B661E', strokeThickness: 1 }).setOrigin(0.5, 0);
+    const text_value = scene.add.text(x, y - y_text_offset, stat.value.toString(), {
+      fontFamily: 'Mochiy Pop One',
+      fontSize: '14px',
+      color: '#fff',
+      stroke: '#8B661E',
+      strokeThickness: 1,
+    }).setOrigin(0.5, 0);
     this.add(text_value);
-    const txt_upgrade = scene.add.text(text_value.x + 30, text_value.y, '(+1)', { fontFamily: 'Mochiy Pop One', fontSize: '14px', color:'#8CFF32', stroke: '#8B661E', strokeThickness: 1, align:'left' }).setOrigin(0.5, 0);
+    const txt_upgrade = scene.add.text(text_value.x + 30, text_value.y, '(+1)', {
+      fontFamily: 'Mochiy Pop One',
+      fontSize: '14px',
+      color: '#8CFF32',
+      stroke: '#8B661E',
+      strokeThickness: 1,
+      align: 'left',
+    }).setOrigin(0.5, 0);
     txt_upgrade.name = 'txt_upgrade';
-    const txt_upgrade_percent = scene.add.text(txt_upgrade.x + 100, txt_upgrade.y, '(1%)', { fontFamily: 'Mochiy Pop One', fontSize: '14px', color:'#8CFF32', stroke: '#8B661E', strokeThickness: 1, align:'left' }).setOrigin(0.5, 0);
+    const txt_upgrade_percent = scene.add.text(txt_upgrade.x + 100, txt_upgrade.y, '(1%)', {
+      fontFamily: 'Mochiy Pop One',
+      fontSize: '14px',
+      color: '#8CFF32',
+      stroke: '#8B661E',
+      strokeThickness: 1,
+      align: 'left',
+    }).setOrigin(0.5, 0);
     txt_upgrade_percent.name = 'txt_upgrade_percent';
     this.add(txt_upgrade);
     this.add(txt_upgrade_percent);
@@ -32,7 +58,7 @@ export default class StatGameObject extends Phaser.GameObjects.Container {
     this.scene.add.existing(this);
   }
 
-  upgrade_status(percent:number) {
+  upgrade_status(percent: number) {
     const txt_upgrade = this.getByName('txt_upgrade') as Phaser.GameObjects.Text;
     txt_upgrade.visible = true;
     const txt_upgrade_percent = this.getByName('txt_upgrade_percent') as Phaser.GameObjects.Text;
@@ -40,7 +66,7 @@ export default class StatGameObject extends Phaser.GameObjects.Container {
     txt_upgrade_percent.visible = true;
   }
 
-  convert_to_short_string(string_type:string) : string {
+  convert_to_short_string(string_type: string): string {
     switch (string_type) {
       case 'attack':
         return 'ATK';
@@ -53,7 +79,7 @@ export default class StatGameObject extends Phaser.GameObjects.Container {
     }
   }
 
-  convert_to_type_to_color(string_type:string) : string {
+  convert_to_type_to_color(string_type: string): string {
     switch (string_type) {
       case 'attack':
         return '#FFBD13';
